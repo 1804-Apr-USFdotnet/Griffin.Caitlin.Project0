a587d52650261b5da6550e7a4ad5a90e79265d26
Caitlin Griffin <Caitlin@Dragongirl.org>
2018-04-22 14:40:17 -0400

    added resturant, resturant reviews, and resturant reviewers classes

--- Project.0/Resturant Library/Resturant.cs
+++ Project.0/Resturant Library/Resturant.cs
@@ -6,7 +6,27 @@ using System.Threading.Tasks;
 
 namespace Resturant_Library
 {
-    public class Class1
+    public class Resturant
     {
+        List <ResturantReviews> ListofReviews { get; set; } //Everything involving a resturant
+        public string ZipCode { get; set; }
+        public string Name { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Street { get; set; }
+        public int StreetNum { get; set; }
+        public int ResturantID { get; set; }
     }
+    public class ResturantReviews // Reviews left by a customer
+    {
+        public int StarReviews { get; set; }
+        public string WrittenReviews { get; set; }
+    
+    }
+    public class ResturantReviewer // The customer itself leaving the review
+    {
+        public string Name { get; set; }
+        public int CustomerID { get; set; }
+    }
+
 }
