ebc5f0e86f725d037efb001876937ae32244ef8f
Caitlin Griffin <Caitlin@Dragongirl.org>
2018-04-23 11:30:48 -0400

    added a palindrome proj

---
+++ PalindromeProj/PalindromeProj/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PalindromeProj
+{
+    class Program
+    {
+        public static bool truePalindrome(string s)
+        {
+            int min = 0;
+            int max = s.Length - 1;
+            while (true)
+            {
+                if (min > max) 
+                    return true;
+
+                if (char.ToLower(s[min++]) != char.ToLower(s[max]))
+                    return false;
+            }
+        static void Main(string[] args)
+        {
+                string[] palindromes =
+                 {
+                    "racecar" , "1221" , "Racecar"
+                };
+          
+                foreach (string value in truePalindrome)
+                {
+                    Console.WriteLine("{0} = {1}", value, truePalindrome(value));
+                }
+
+                Console.WriteLine("\nPress any key to continue...");
+                Console.ReadKey(true);
+            }
+        }
+
+    }
+}
+}
---
+++ PalindromeProj/PalindromeTesting/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("PalindromeTesting")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PalindromeTesting")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("317c0381-6529-40ef-8a98-528c885156d4")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
